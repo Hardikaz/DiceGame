73f88608aa1cf78a0defed409e3257c3f84ff43f
Hardik Sharma <sharmahardikaz@gmail.com>
2023-06-28 16:35:42 +0530

    Done with the javascript part.The game is working now

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ var imgNumber2=Math.ceil(Math.random()*6);
 var newUrl2="images/dice"+imgNumber2+".png";
 console.log(newUrl2);
 document.querySelectorAll("img")[1].setAttribute("src",newUrl2);
+
+//Main game code
+
+if(imgNumber>imgNumber2){
+    //Play sound
+    //show some animations
+    document.querySelector("h1").innerHTML="PLayer 1 won the match";
+}
+if(imgNumber<imgNumber2){
+    //Play sound
+    //show some animations
+    document.querySelector("h1").innerHTML="PLayer 2 won the match";
+}
+if(imgNumber===imgNumber2){
+    //Play sound
+    //show some animations
+    document.querySelector("h1").innerHTML="It's a draw";
+}
