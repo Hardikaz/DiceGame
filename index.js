07d302728d0c3dbb342236ee84a4a7fba8b38e4f
Hardik Sharma <sharmahardikaz@gmail.com>
2023-07-05 00:34:25 +0530

    Impemented another javascript

--- index.js
+++ index.js
@@ -48,6 +48,19 @@ function rollDice(){
     var url2="images/dice"+dice2+".png";
     
     document.querySelectorAll("img")[1].setAttribute("src",url2);
+
+    if(dice1>dice2)
+    {
+        document.querySelector('h1').innerHTML="Player 1 earned a point";
+    }
+    if(dice1<dice2)
+    {
+        document.querySelector('h1').innerHTML="Player 2 earned a point";
+    }
+    if(dice1==dice2)
+    {
+        document.querySelector('h1').innerHTML="It's a draw";
+    }
     
 }
 
